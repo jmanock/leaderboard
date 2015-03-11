74f68f2fb2e0cacaa6c2890ec95b69c10b6d3e2e
jmanock <jonmanock@gmail.com>
2015-03-11 15:01:59 -0400

    refactoring

--- app/pageLeaderboard.js
+++ app/pageLeaderboard.js
@@ -1,42 +1,20 @@
-var LEADERBOARD_SIZE = 100;
-
-// Create our Firebase reference
-var scoreListRef = new Firebase('https://toga.firebaseio.com/');
-
-// Keep a mapping of firebase locations to HTML elements, so we can move/ remove elements as necessary.
+var scoreList = new Firebase('https://toga.firebaseio.com');
 var htmlForPath = {};
 
-// Helper function that takes a new score snapshot and adds an appropriate row to our leaderboard table.
-function handleScoreAdded(scoreSnapshot, prevScoreName){
-  var newScoreRow = $('<tr/>');
-  newScoreRow.append($('<td/>').append($('<em/>').text(scoreSnapshot.val().Name)));
-  newScoreRow.append($('<td/>').text(scoreSnapshot.val().Score));
-  newScoreRow.append($('<td/>').text(scoreSnapshot.val().Total));
-  newScoreRow.append($('<button class="button">Add</button>'));
+function newScore(score, prev){
+  var scoreRow = $('<tr/>');
+  scoreRow.append($('<td/>').append($('<em>').text(score.val().Name)));
+  scoreRow.append($('<td/>').text(score.val().Total));
 
-  // Store a reference to the table row so we can get it later
-  htmlForPath[scoreSnapshot.key()] = newScoreRow;
-  // Insert the new score in the appropriate place in the table.
-  if(prevScoreName === null){
-    $('#leaderboardTable').append(newScoreRow);
+  htmlForPath[score.key()]= scoreRow;
+  if(prev === null){
+    $('#leaderboardTable').append(scoreRow);
   }else{
-    var lowerScoreRow = htmlForPath[prevScoreName];
-    lowerScoreRow.after(newScoreRow);
+    var lowerScoreRow = htmlForPath[prev];
+    lowerScoreRow.after(scoreRow);
   }
-  
 }
-
-
-// Create a view to only recieve callbacks for the last LEADERBOARD_SIZE scores
-var scoreListView = scoreListRef.limitToLast(LEADERBOARD_SIZE);
-
-// Add a callback to handle when a new score is added
-scoreListView.on('child_added', function(newScoreSnapshot, prevScoreName){
-  handleScoreAdded(newScoreSnapshot, prevScoreName);
+var scoreListView = scoreList.limitToLast(200);
+scoreListView.on('child_added', function(score, prev){
+  newScore(score,prev);
 });
-
-// Add a callback to handle when a score changes or moves positions
-var changedCallback = function(scoreSnapshot, prevScoreName){
-  handleScoreRemoved(scoreSnapshot);
-  handleScoreAdded(scoreSnapshot, prevScoreName);
-};
