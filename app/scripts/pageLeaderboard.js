1def26ffd913cb2afd8e5447eb031e3ce1ee8754
jmanock <jonmanock@gmail.com>
2015-03-16 16:37:34 -0400

    moving files around

--- app/scripts/pageLeaderboard.js
+++ app/scripts/pageLeaderboard.js
@@ -1,6 +1,11 @@
 var app = angular.module('app', ['firebase']);
 
-app.controller('PlayerCtrl', function($scope, $firebaseObject){
+app.controller('MainCtrl', function($scope, $firebaseArray){
+  var ref = new Firebase('https://reditclone.firebaseio.com');
+  $scope.golfers = $firebaseArray(ref);
+});
+
+app.controller('PlayerCtrl', function($scope, $firebaseArray){
   var ref = new Firebase('https://toga.firebaseio.com');
-  $scope.players = $firebaseObject(ref);
+  $scope.players = $firebaseArray(ref);
 });
