8c0e34581e7f6a6226f464199bb2bde5ce64bcb8
jmanock <jonmanock@gmail.com>
2015-03-26 06:47:31 -0400

    players showing up

--- app/FbLogin/profile.js
+++ app/FbLogin/profile.js
@@ -1,5 +1,8 @@
 'use strict';
 angular.module('Leaderboard')
-  .controller('ProfileCtrl',function(){
-    
+  .controller('ProfileCtrl',function($scope, $firebaseObject){
+    var ref = new Firebase('https://toga.firebaseio.com');
+    var syncObject = $firebaseObject(ref);
+    syncObject.$bindTo($scope, 'profile.players');
+
   });
